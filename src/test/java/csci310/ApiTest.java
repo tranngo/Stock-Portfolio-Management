d276f03de950be0aea9ac913145e442977e365c6
maggieshi <maggieshi@192.168.68.100>
2020-10-07 20:19:14 -0700

    minor cleanup change

--- src/test/java/csci310/ApiTest.java
+++ src/test/java/csci310/ApiTest.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
