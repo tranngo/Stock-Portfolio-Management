d90e967a489d51b91ea83131bca2465f0af4c266
Sai Allu <allu@usc.edu>
2020-10-21 12:42:47 -0400

    Adding state machine code

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -8,6 +8,94 @@ google.charts.load("current", { packages: ["corechart"] });
     ["2006", 660, 1120],
     ["2007", 1030, 540],
   ];
+  
+  //These are the state variables which affect the graph, setting default values
+  state_portfolioContributors = [];
+  state_externalStocks = ["NTNX", "JNJ", "FB", "TSLA"];
+  state_start = "2020-01-01";
+  state_end = "2020-06-01";
+  
+  //Calling this function will take the "state" and pass it to GraphServlet as your request
+  function refreshGraph() {
+
+    console.log("Graph refresh requested");
+    console.log("State variable -> Start date: " + state_start);
+    console.log("State variable -> End date: " + state_end);
+    
+    state_portfolioContributors_asAString = "";
+    state_externalStocks_asAString = "";
+    
+    //Turn portfolio contributors from an array into a string
+    var i = 0;
+	for (i = 0; i < state_portfolioContributors.length; i++) { 
+	  state_portfolioContributors_asAString += state_portfolioContributors[i] + ",";
+	}
+    
+    //Turn external stocks from an array into a string
+    var j = 0;
+	for (j = 0; j < state_externalStocks.length; j++) { 
+	  state_externalStocks_asAString += state_externalStocks[j] + ",";
+	}
+    
+    console.log("State variable (but as a string) -> Portfolio contributors: " + state_portfolioContributors_asAString);
+    console.log("State variable (but as a string) -> External stocks: " + state_externalStocks_asAString);
+    
+    //Eventually this part of the code will be replaced by state update and refreshGraph()
+    $.ajax({
+      url: "GraphServlet",
+      type: "GET",
+      data: {
+      	startDate: state_start,
+      	endDate: state_end,
+      	portfolioContributors: state_portfolioContributors_asAString,
+      	externalStocks: state_externalStocks_asAString
+      },
+
+      success: function (result) {
+        jsonArray = eval(result);
+        console.log(jsonArray);
+
+        for (let i = 1; i < jsonArray.length; ++i) {
+          for (let j = 1; j < jsonArray[i].length; ++j) {
+            if (jsonArray[i][j] === "null") {
+              jsonArray[i][j] = null;
+            } else {
+              jsonArray[i][j] = parseInt(jsonArray[i][j]);
+            }
+          }
+        }
+        
+        drawMainChart();
+      },
+    });
+    return false;
+  }
+  
+  //Add to portfolio contributors
+  function addPortfolioContributor(s) {
+  	state_portfolioContributors.push(s);
+  	refreshGraph();
+  }
+  
+  //Remove from portfolio contributors
+ 
+  //Add an external stock
+  
+  //Remove an external stock
+  
+  //Change the start date
+  
+  //Change the end date
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
   function drawMainChart() {
     var data = google.visualization.arrayToDataTable(jsonArray);
