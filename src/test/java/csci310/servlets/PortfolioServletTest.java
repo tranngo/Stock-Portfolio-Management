cf7ba6313cfac6233cbc945fed8bc5b8e51ba7de
Sai Allu <allu@usc.edu>
2020-10-27 16:45:21 -0400

    Adding assertTrue to PortfolioServletTest

--- src/test/java/csci310/servlets/PortfolioServletTest.java
+++ src/test/java/csci310/servlets/PortfolioServletTest.java
@@ -1,10 +1,12 @@
 package csci310.servlets;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,16 +20,31 @@ public class PortfolioServletTest extends Mockito {
 		HttpServletRequest request = mock(HttpServletRequest.class);
 		HttpServletResponse response = mock(HttpServletResponse.class);
 		PortfolioServlet ps = new PortfolioServlet();
+		
+		//User id is 50
+		Cookie[] cookies = new Cookie[1];
+		cookies[0] = new Cookie("user_id", "50");
+		when(request.getCookies()).thenReturn(cookies);
 
+		//Test add
+		when(request.getParameter("type")).thenReturn("add");
+		when(request.getParameter("stock")).thenReturn("NTNX");
+		when(request.getParameter("quantity")).thenReturn("50");
+		when(request.getParameter("buyDate")).thenReturn("09-09-2019");
+		when(request.getParameter("sellDate")).thenReturn("09-18-2019");
+		
 		when(response.getWriter()).thenReturn(new PrintWriter("Yo"));
 		ps.doPost(request, response);
-
-
-		//Test for null values as input
-		when(request.getParameter("startDate")).thenReturn(null);
-		when(request.getParameter("endDate")).thenReturn(null);
-		when(request.getParameter("externalStocks")).thenReturn(null);
+		assertTrue(response != null);
+		
+		//Test remove
+		when(request.getParameter("type")).thenReturn("remove");
+		when(request.getParameter("stock")).thenReturn("NTNX");
+		
+		when(response.getWriter()).thenReturn(new PrintWriter("Yo"));
 		ps.doPost(request, response);
+		assertTrue(response != null);
+		
 	}
 
 }
