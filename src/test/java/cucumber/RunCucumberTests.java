e18b01fea664d5eb47cb49821431dd95851f7070
Mark Kyaw <annonymous10101@gmail.com>
2020-11-12 17:21:42 -0800

    add a line to run all tests at once

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -12,6 +12,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
 //Please run one at a time, just in case :)
+//@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement2.feature", "src/test/resources/cucumber/requirement3.feature", 
+//		"src/test/resources/cucumber/requirement4.feature", "src/test/resources/cucumber/requirement5.feature", "src/test/resources/cucumber/requirement6.feature", 
+//		"src/test/resources/cucumber/requirement7_login.feature", "src/test/resources/cucumber/requirement7_register.feature", "src/test/resources/cucumber/requirement8.feature"})
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement2.feature"})
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement3.feature"})
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement4.feature"})
@@ -19,7 +22,7 @@ import org.junit.runner.RunWith;
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement6.feature"})
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement7_login.feature"})
 //@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement7_register.feature"})
-@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement8.feature"})
+//@CucumberOptions(strict = true, features = {"src/test/resources/cucumber/requirement8.feature"})
 
 public class RunCucumberTests {
 
